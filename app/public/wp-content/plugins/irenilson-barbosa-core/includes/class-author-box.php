34cae5d3bb9232c2fe1bb8b63a8d94d38d5f183b
Tarcísio Barbosa <oi@tarcisiobarbosa.com>
2026-07-20 14:56:39 +0100

    feat: author-box com link para página /sobre/

--- app/public/wp-content/plugins/irenilson-barbosa-core/includes/class-author-box.php
+++ app/public/wp-content/plugins/irenilson-barbosa-core/includes/class-author-box.php
@@ -24,19 +24,22 @@ class AuthorBox {
 		$author_name = get_the_author();
 		$author_bio  = get_the_author_meta('description');
 		$author_avatar = get_avatar($author_id, 80, '', $author_name, ['class' => 'avatar']);
+		$about_link = home_url('/sobre/');
 
 		return sprintf(
 			'<div class="ib-author-box">
 				<div class="ib-author-box__inner">
-					<div class="ib-author-box__avatar">%s</div>
+					<div class="ib-author-box__avatar"><a href="%s">%s</a></div>
 					<div class="ib-author-box__body">
 						<span class="ib-author-box__label">Sobre o autor</span>
-						<strong class="ib-author-box__name">%s</strong>
+						<strong class="ib-author-box__name"><a href="%s" style="color:inherit;text-decoration:none">%s</a></strong>
 						<p class="ib-author-box__bio">%s</p>
 					</div>
 				</div>
 			</div>',
+			esc_url($about_link),
 			$author_avatar,
+			esc_url($about_link),
 			esc_html($author_name),
 			esc_html($author_bio)
 		);
