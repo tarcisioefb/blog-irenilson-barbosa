6e6470053e0b7cb891b5a7857a2d6392f8a9ca53
Tarcísio Barbosa <oi@tarcisiobarbosa.com>
2026-07-22 17:26:15 +0100

    fix: modal Ver detalhes via plugins_api + plugin_row_meta

--- app/public/wp-content/plugins/irenilson-barbosa-core/irenilson-barbosa-core.php
+++ app/public/wp-content/plugins/irenilson-barbosa-core/irenilson-barbosa-core.php
@@ -57,3 +57,40 @@ if (defined('WP_CLI') && WP_CLI) {
 	\WP_CLI::add_command('ib-convert', ['\IrenilsonBarbosa\Core\ImageOptimizer', 'cli_convert']);
 	\WP_CLI::add_command('ib-thumb-avif', ['\IrenilsonBarbosa\Core\ImageOptimizer', 'cli_thumb_avif']);
 }
+
+add_filter('plugins_api', function ($result, $action, $args) {
+	if ($action !== 'plugin_information' || ($args->slug ?? '') !== 'irenilson-barbosa-core') return $result;
+	$readme = IRENILSON_CORE_PATH . 'readme.txt';
+	if (!file_exists($readme)) return $result;
+
+	$content = file_get_contents($readme);
+	preg_match('/^=== (.+?) ===/m', $content, $name);
+	preg_match('/^Stable tag: (.+)/m', $content, $ver);
+	preg_match('/^Contributors: (.+)/m', $content, $contrib);
+	preg_match('/^== Description ==\n(.+?)\n==/ms', $content, $desc);
+	preg_match('/^== Installation ==\n(.+?)\n==/ms', $content, $install);
+	preg_match('/^== Changelog ==\n(.+?)$/ms', $content, $changelog);
+
+	return (object) [
+		'name'            => $name[1] ?? 'Irenilson Barbosa Core',
+		'slug'            => 'irenilson-barbosa-core',
+		'version'         => $ver[1] ?? IRENILSON_CORE_VERSION,
+		'author'          => '<a href="https://zucatech.com">Zucatech</a>',
+		'requires'        => '6.0',
+		'tested'          => '7.0',
+		'last_updated'    => gmdate('Y-m-d'),
+		'sections'        => [
+			'description' => nl2br($desc[1] ?? ''),
+			'installation' => nl2br($install[1] ?? ''),
+			'changelog'    => nl2br($changelog[1] ?? ''),
+		],
+		'short_description' => 'Gerencia CPTs, SEO, LGPD, TTS, newsletter e segurança do portal editorial.',
+		'banners'         => [],
+	];
+}, 10, 3);
+
+add_filter('plugin_row_meta', function ($meta, $file) {
+	if ($file !== plugin_basename(IRENILSON_CORE_PATH . 'irenilson-barbosa-core.php')) return $meta;
+	$meta[] = '<a href="' . self_admin_url('plugin-install.php?tab=plugin-information&plugin=irenilson-barbosa-core&TB_iframe=true&width=600&height=550') . '" class="thickbox open-plugin-details-modal" aria-label="Ver detalhes">Ver detalhes</a>';
+	return $meta;
+}, 10, 2);
